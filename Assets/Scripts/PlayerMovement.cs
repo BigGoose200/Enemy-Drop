200da1ad7ea82079962ac043f6793b4debfc86a1
BigGoose200 <60943774+BigGoose200@users.noreply.github.com>
2020-09-02 02:19:53 -0400

    added assets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; // how fast the player goes
+    private float horizontal; // controls horizontal axis
+    public float jumpForce; // how high the player jumps
+    public bool grounded = false; // checks to see if player is touching ground
+    public GameObject groundCheck = null; // slot for the ground checker
+    private Rigidbody2D MyRb; // need a body to jump
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRb = GetComponent<Rigidbody2D>(); //connect ref to rigidbody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gives the player horizontal movement
+        horizontal = Input.GetAxisRaw("Horizontal");
+        MyRb.velocity = new Vector2(horizontal * speed, MyRb.velocity.y);
+
+        // checks to see if the player and groundCheck are touching a platform
+        if (Physics2D.Linecast(transform.position, groundCheck.transform.position))
+        {
+            grounded = true; // allows the player to jump
+        }
+        else // runs while player is in the air
+        {
+            grounded = false; // prevents the player from jumping
+        }
+
+        //if the player presses space while grounded, he or she jumps
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+}
---
+++ Assets/Scripts/ShipControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+	public GameObject orbPrefab; // var to hold prefab for instantiating orbs
+	public float speed = 10f; // speed ship is moving
+	float leftRightEdges = 8f; // distance before the ship changes direction
+	float chanceDirectionChange = 0.04f; // how likely ship will change direction
+	public float secsBetweenLaunch = 4f; // rate that we generate orbs from the ship
+
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("LaunchOrb", 2f, secsBetweenLaunch);  //calls a function every x secs 2f from start of game
+	}
+	void LaunchOrb()
+	{
+		GameObject orb = Instantiate(orbPrefab) as GameObject; // create reference to hold game object
+		orb.transform.position = transform.position;
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 pos = transform.position; // create a var to hold current position
+		pos.x += speed * Time.deltaTime; // sets the xpos of our ship to the speed var * sec since last frame
+		transform.position = pos;
+
+		if (pos.x < -leftRightEdges)
+		{ // if the ship pos is less than -10 set speed to a pos number
+			speed = Mathf.Abs(speed);
+		}
+		else if (pos.x > leftRightEdges)
+		{
+			speed = -Mathf.Abs(speed);  // if the ship pos x is greater than 10 reverse speed
+		}
+	}
+	void FixedUpdate()
+	{
+		if (Random.value < chanceDirectionChange)
+		{  // change direction at a random interval
+			speed *= -1;
+
+		}
+	}
+}
